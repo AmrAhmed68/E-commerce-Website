de1783002d4ea67162aa47065a6259489867722c
Ahmed <aw172759@gmail.com>
2023-12-15 16:54:45 +0200

    wael

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors'); 
-const User=require("./Models/User");
+const cors = require('cors');
+const User = require("./Models/User");
 const Product = require('./Models/Product');
 const bcrypt = require('bcryptjs');
 const authP = require('./checkAuth/auth');
@@ -12,7 +12,41 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.post('/signup', async (req, res) => {
-    const {
+  const {
+    username,
+    password,
+    confirmPassword,
+    email,
+    phoneNumber,
+    age,
+    country,
+  } = req.body;
+
+  try {
+    // Check if the email is a valid Gmail address
+    if (!email.endsWith('@gmail.com')) {
+      return res.status(400).json({ error: 'Email must be a valid Gmail address.' });
+    }
+
+    // Check if the password and confirmPassword match
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: 'Passwords do not match' });
+    }
+
+    // Check if the username is already in use
+    const usernameExists = await User.findOne({ username });
+    if (usernameExists) {
+      return res.status(400).json({ error: 'Username is already in use.' });
+    }
+
+    // Check if the email is already registered
+    const emailExists = await User.findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({ error: 'Email is already registered' });
+    }
+
+    // Create a new user instance
+    const newUser = new User({
       username,
       password,
       confirmPassword,
@@ -20,134 +54,100 @@ app.post('/signup', async (req, res) => {
       phoneNumber,
       age,
       country,
-    } = req.body;
-  
-    try {
-      // Check if the email is a valid Gmail address
-      if (!email.endsWith('@gmail.com')) {
-        return res.status(400).json({ error: 'Email must be a valid Gmail address.' });
-      }
-  
-      // Check if the password and confirmPassword match
-      if (password !== confirmPassword) {
-        return res.status(400).json({ error: 'Passwords do not match' });
-      }
-  
-      // Check if the username is already in use
-      const usernameExists = await User.findOne({ username });
-      if (usernameExists) {
-        return res.status(400).json({ error: 'Username is already in use.' });
-      }
-  
-      // Check if the email is already registered
-      const emailExists = await User.findOne({ email });
-      if (emailExists) {
-        return res.status(400).json({ error: 'Email is already registered' });
-      }
-  
-      // Create a new user instance
-      const newUser = new User({
-        username,
-        password,
-        confirmPassword,
-        email,
-        phoneNumber,
-        age,
-        country,
-      });
-  
-      
-      await newUser.save();
-  
-      
-      const token = newUser.genAuthToken();
-  
+    });
 
-      return res.header('x-auth-token', token).json({
-        message: 'Sign-up successful!',
-        token,
-      });
-    } catch (error) {
-      console.error('Error during sign-up:', error);
-      return res.status(500).json({ error: 'An error occurred during sign-up' });
-    }
-  });
-  
-  
+
+    await newUser.save();
+
+
+    const token = newUser.genAuthToken();
+
+
+    return res.header('x-auth-token', token).json({
+      message: 'Sign-up successful!',
+      token,
+    });
+  } catch (error) {
+    console.error('Error during sign-up:', error);
+    return res.status(500).json({ error: 'An error occurred during sign-up' });
+  }
+});
+
+
 app.post('/Login', async (req, res) => {
-    const { email, password  , Username , PhoneNumber , age , country } = req.body;
+  const { email, password, Username, PhoneNumber, age, country } = req.body;
 
-    try {
-        const user = await User.findOne({ email });
+  try {
+    const user = await User.findOne({ email });
 
-        if (!user) {
-            return res.status(400).send('Invalid username or password');
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-
-        if (!isPasswordValid) {
-            return res.status(400).send('Invalid Email or password');
-        }
-        const newUser = new User({
-          Username,
-          password,
-          email,
-          PhoneNumber,
-          age,
-          country,
-        });
-
-        const token = user.genAuthToken();
-        res.header('x-auth-token', token).send('Login successful');
-    }catch (error) {
-        console.error('Error during login:', error);
-        return res.status(500).send('Internal Server Error');
+    if (!user) {
+      return res.status(400).send('Invalid username or password');
     }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).send('Invalid Email or password');
+    }
+    const newUser = new User({
+      Username,
+      password,
+      email,
+      PhoneNumber,
+      age,
+      country,
+    });
+
+    const token = user.genAuthToken();
+    res.header('x-auth-token', token).send('Login successful');
+  } catch (error) {
+    console.error('Error during login:', error);
+    return res.status(500).send('Internal Server Error');
+  }
 });
 
 app.get('/profile', authMid, (req, res) => {
-    return res.json(req.user);
+  return res.json(req.user);
 });
 
 //product
 
-app.post('/admin/add-product', authMid , async (req, res) => {
-    const { title , description , price ,  brand , images} = req.body;
+app.post('/admin/add-product', authMid, async (req, res) => {
+  const { title, description, price, brand, images } = req.body;
 
-    try {
-        let product = new Product({ title , description , price ,  brand , images});
-        product = await product.save();
+  try {
+    let product = new Product({ title, description, price, brand, images });
+    product = await product.save();
 
-        return res.send(product);
-    } catch (error) {
-        console.error('Error adding product:', error);
-        return res.status(500).send('Internal Server Error');
-    }
+    return res.send(product);
+  } catch (error) {
+    console.error('Error adding product:', error);
+    return res.status(500).send('Internal Server Error');
+  }
 });
 
 app.delete('/admin/delete-product/:productId', authMid, async (req, res) => {
-    try {
+  try {
 
-        const productId = req.params.productId;
+    const productId = req.params.productId;
 
-        // Ensure the productId is valid (you might want to add more validation here)
-        if (!mongoose.Types.ObjectId.isValid(productId)) {
-            return res.status(400).send('Invalid product ID');
-        }
-
-        // Attempt to delete the product
-        const deletedProduct = await Product.findByIdAndDelete(productId);
-
-        if (!deletedProduct) {
-            return res.status(404).send('Product not found');
-        }
-
-        return res.send(deletedProduct);
-    } catch (error) {
-        console.error('Error deleting product:', error);
-        return res.status(500).send('Internal Server Error');
+    // Ensure the productId is valid (you might want to add more validation here)
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+      return res.status(400).send('Invalid product ID');
     }
+
+    // Attempt to delete the product
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+
+    if (!deletedProduct) {
+      return res.status(404).send('Product not found');
+    }
+
+    return res.send(deletedProduct);
+  } catch (error) {
+    console.error('Error deleting product:', error);
+    return res.status(500).send('Internal Server Error');
+  }
 });
 
 app.put('/:id', authMid, async (req, res) => {
@@ -180,25 +180,25 @@ app.put('/:id', authMid, async (req, res) => {
 });
 
 app.get('/product', async (req, res) => {
-    try {
-      const products = await Product.find();
-      return res.send(products);
-    } catch (error) {
-      console.error('Error fetching products:', error);
-      return res.status(500).send('Internal Server Error');
-    }
-  });
+  try {
+    const products = await Product.find();
+    return res.send(products);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return res.status(500).send('Internal Server Error');
+  }
+});
 
-  app.get('/products/:brand', async (req, res) => {
-    const productbrand = req.params.brand;
-    try {
-      const lenovoProducts = await Product.find({ brand: productbrand });
-      res.json(lenovoProducts);
-    } catch (error) {
-      console.error('Error fetching Lenovo products:', error.message);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+app.get('/products/:brand', async (req, res) => {
+  const productbrand = req.params.brand;
+  try {
+    const lenovoProducts = await Product.find({ brand: productbrand });
+    res.json(lenovoProducts);
+  } catch (error) {
+    console.error('Error fetching Lenovo products:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.get('/:id', async (req, res) => {
   try {
@@ -219,58 +219,58 @@ app.get('/:id', async (req, res) => {
 
 //card 
 app.get('/cart', authP, async (req, res) => {
-    let products = [];
+  let products = [];
 
-    for (const product of req.user.cart)
-        products.push(await Product.findById(product._id));
+  for (const product of req.user.cart)
+    products.push(await Product.findById(product._id));
 
-    return res.send(products);
+  return res.send(products);
 });
 
 app.put('/addproduct', authP, async (req, res) => {
-    const { id } = req.body;
-    if (!id) return res.status(400).send('Product id is required');
+  const { id } = req.body;
+  if (!id) return res.status(400).send('Product id is required');
 
-    const product = await Product.findById(id);
-    if (!product) return res.status(404).send('The product you are looking for is not found');
+  const product = await Product.findById(id);
+  if (!product) return res.status(404).send('The product you are looking for is not found');
 
-    if (req.user.cart.includes(product._id)) return res.status(400).send('The product is already in your cart');
+  if (req.user.cart.includes(product._id)) return res.status(400).send('The product is already in your cart');
 
-    const user = await User.findByIdAndUpdate(req.user._id, {cart: [...req.user.cart, product._id]}, 
-        { new: true, useFindAndModify: false });
+  const user = await User.findByIdAndUpdate(req.user._id, { cart: [...req.user.cart, product._id] },
+    { new: true, useFindAndModify: false });
 
-    return res.send(user);
+  return res.send(user);
 });
 
 app.delete('/:id', authP, async (req, res) => {
-    const { id } = req.params;
-    req.user.cart = req.user.cart.filter(p => String(p) !== id);
-    return res.send(await req.user.save());
+  const { id } = req.params;
+  req.user.cart = req.user.cart.filter(p => String(p) !== id);
+  return res.send(await req.user.save());
 });
 
 app.delete('/', authP, async (req, res) => {
-    req.user.cart = [];
-    return res.send(await req.user.save());
+  req.user.cart = [];
+  return res.send(await req.user.save());
 });
 
 app.get('/total', authP, async (req, res) => {
-    let total = 0;
+  let total = 0;
 
-    let products = [];
-    for (const product of req.user.cart)
-        products.push(await Product.findById(product));
+  let products = [];
+  for (const product of req.user.cart)
+    products.push(await Product.findById(product));
 
-    for (const product of products)
-        total += product.price;
+  for (const product of products)
+    total += product.price;
 
-    return res.send(String(total));
-    
+  return res.send(String(total));
+
 });
 
 mongoose.connect('mongodb+srv://amr682003:AmrA682003@cluster0.mjxdqxw.mongodb.net/?retryWrites=true&w=majority')
-    .then(() => {
-        console.log('connected to MongoDB')
-        app.listen(9000, () => console.log('app started on port 9000'))
-    }).catch((error) => {
-        console.log('cant connect to mongodb' + error)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+    app.listen(9000, () => console.log('app started on port 9000'))
+  }).catch((error) => {
+    console.log('cant connect to mongodb' + error)
+  })
